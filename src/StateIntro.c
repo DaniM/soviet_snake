544ccf357dfde9e6495ac35183c6283f0b63ad18
Dani <danmaarqui@gmail.com>
2019-07-28 18:29:25 +0200

    jarl

--- src/StateIntro.c
+++ src/StateIntro.c
@@ -10,14 +10,14 @@
 #include "Print.h"
 #include "Keys.h"
 
-
+extern UINT8* russia_mod_Data[]; 
 void Start_StateIntro() {
 
 	//INIT_CONSOLE(font, 2);
 	InitScrollTiles(0, &font, bank_tiles);
 	InitScroll(introWidth, introHeight, intro, 0, 0, 3);
 	SHOW_BKG;
-
+	PlayMusic(russia_mod_Data, 3, 1);
 }
 
 void Update_StateIntro() {
